fbee4305e3160437db776f1d19b1a029cb52a071
mohammedtohamym <mohammedtohamym@gmail.com>
2024-06-18 15:28:47 -0700

    javascript tasks

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const c = 'C is fun';
+const python = 'Python is cool';
+const javascript = 'JavaScript is amazing';
+const languages = [c, python, javascript];
+for (let i = 0; i < 3; i++) {
+  console.log(languages[i]);
+}
